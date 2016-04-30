9edca5c1f2fe0ebaeddf133cef9d3e3db284ffc6
kavyaalse <kavyaalse@gmail.com>
2016-04-30 16:01:35 +0530

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,2 +1,23 @@
 //This file will have the UI related js functions
 
+var agentPlantA = {
+  properties : {
+    property1 {
+      name: Plant Height;
+      value: {"TT", "Tt", "tt"};
+    };  
+    property2 {
+      name: Seed Color;
+      value: {"YY", "Yy", "yy"};
+    };
+    property3 {
+      name: Seed Shape;
+      value: "null";
+    }
+    property4 {
+      name: Flower Color;
+      value: "null";
+    }
+  }
+}
+
