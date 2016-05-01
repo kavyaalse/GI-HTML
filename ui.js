8d41a988781d2379436688bd31c3de0694a47e53
kavyaalse <kavyaalse@gmail.com>
2016-05-01 10:25:27 +0530

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,23 +1,59 @@
 //This file will have the UI related js functions
 
 var agentPlantA = {
-  properties : {
-    property1 {
-      name: Plant Height;
-      value: {"TT", "Tt", "tt"};
-    };  
-    property2 {
-      name: Seed Color;
-      value: {"YY", "Yy", "yy"};
-    };
-    property3 {
-      name: Seed Shape;
-      value: "null";
+  properties : [
+    {
+      name: "Plant Height",
+      value: ["TT", "Tt", "tt"],
+    }, 
+    {
+      name: "Seed Color",
+      value: ["YY", "Yy", "yy"]
+    },
+    {
+      name: "Seed Shape",
+      value: "null"
+    },
+    {
+      name: "Flower Color",
+      value: "null"
     }
-    property4 {
-      name: Flower Color;
-      value: "null";
-    }
-  }
-}
+  ],
+  behaviour : [
+    "Growth in Height"
+  ]
 
+};
+
+$( document ).ready(function() {
+
+    $( "#addAgentButton" ).click(function() {
+        $( "#addAgentDialog" ).html("<fieldset><input type=\"checkbox\" name=\"selectAgent\" id=\"selectedPlantA\" value=\"plantA\" /> Plant A <br /><input type=\"checkbox\" name=\"selectAgent\" id=\"selectedPlantB\" value=\"plantB\" /> Plant B <br /> </fieldset>");
+        $( "#addAgentDialog" ).dialog({
+        dialogClass: "no-close",
+        resizable: true,
+        modal: true,
+        title: "Select Agents",
+        buttons: [
+          {
+            text: "OK",
+            click: function() {
+              $( this ).dialog( "close" );
+            }
+          }
+        ],
+        open: function(event, ui) {
+        $(this).parent().css('position','fixed');
+        }
+      });
+        $( "#addAgentDialog" ).dialog(open);
+    });
+
+    if ($('#selectedPlantA').is(':checked')) {
+      $( "#addAgentA" ).html("");
+    }
+
+});
+
+
+    
